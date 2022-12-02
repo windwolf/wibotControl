5048a4d99007539f1956eae9290dfccd20dfa780
zhoujian <zhoujian.ww@gmail.com>
2022-12-02 21:49:48 +0800

    增加pid

--- pid/pid.cpp
+++ pid/pid.cpp
@@ -3,28 +3,33 @@
 namespace wwControl
 {
 
-void PidController::reset()
-{
-    /* Clear controller variables */
-    _integrator = 0.0f;
-    _prevError = 0.0f;
+	void PidController::reset()
+	{
+		/* Clear controller variables */
+		_integrator = 0.0f;
+		_prevError = 0.0f;
 
-    _differentiator = 0.0f;
-    _prevMeasurement = 0.0f;
-};
+		_differentiator = 0.0f;
+		_prevMeasurement = 0.0f;
+	};
 
-float PidController::update(float setpoint, float measurement)
-{
-    switch (config.mode)
-    {
-    case Mode::Serial:
-        return update_serial(setpoint, measurement);
-    case Mode::Parallel:
-        return update_parallel(setpoint, measurement);
-    default:
-        return 0.0f;
-    }
-};
+	void _config_apply()
+	{
+
+	};
+
+	float PidController::update(float setpoint, float measurement)
+	{
+		switch (_config.mode)
+		{
+		case PidControllerMode::Serial:
+			return update_serial(setpoint, measurement);
+		case PidControllerMode::Parallel:
+			return update_parallel(setpoint, measurement);
+		default:
+			return 0.0f;
+		}
+	};
 
 /**
  * @brief y = Kp + Ki/s + Kd*s/(s*tua+1)
@@ -36,73 +41,73 @@ float PidController::update(float setpoint, float measurement)
  * @param measurement
  * @return float
  */
-float PidController::update_parallel(float setpoint, float measurement)
-{
-    float out;
+	float PidController::update_parallel(float setpoint, float measurement)
+	{
+		float out;
 
-    /*
-     * Error signal
-     */
-    float error = setpoint - measurement;
+		/*
+		 * Error signal
+		 */
+		float error = setpoint - measurement;
 
-    /*
-     * Proportional
-     */
-    float proportional = config.Kp * error;
+		/*
+		 * Proportional
+		 */
+		float proportional = _config.Kp * error;
 
-    /*
-     * Integral
-     */
-    _integrator = _integrator + 0.5f * config.Ki * config.sample_time * (error + _prevError);
+		/*
+		 * Integral
+		 */
+		_integrator = _integrator + 0.5f * _config.Ki * _config.sample_time * (error + _prevError);
 
-    /* Anti-wind-up via _integrator clamping */
-    if (config.intergrator_limit_enable)
-    {
-        if (_integrator > config.intergrator_limit_max)
-        {
-            _integrator = config.intergrator_limit_max;
-        }
-        else if (_integrator < config.intergrator_limit_min)
-        {
-            _integrator = config.intergrator_limit_min;
-        }
-    }
+		/* Anti-wind-up via _integrator clamping */
+		if (_config.integrator_limit_enable)
+		{
+			if (_integrator > _config.integrator_limit_max)
+			{
+				_integrator = _config.integrator_limit_max;
+			}
+			else if (_integrator < _config.integrator_limit_min)
+			{
+				_integrator = _config.integrator_limit_min;
+			}
+		}
 
-    /*
-     * Derivative (band-limited _differentiator)
-     */
+		/*
+		 * Derivative (band-limited _differentiator)
+		 */
 
-    _differentiator =
-        -(2.0f * config.Kd *
-              (measurement - _prevMeasurement) /* Note: derivative on measurement, therefore
+		_differentiator =
+			-(2.0f * _config.Kd *
+				(measurement - _prevMeasurement) /* Note: derivative on measurement, therefore
                                                         minus sign in front of equation! */
-          + (2.0f * config.tau - config.sample_time) * _differentiator) /
-        (2.0f * config.tau + config.sample_time);
+				+ (2.0f * _config.tau - _config.sample_time) * _differentiator) /
+				(2.0f * _config.tau + _config.sample_time);
 
-    /*
-     * Compute output and apply limits
-     */
-    out = proportional + _integrator + _differentiator;
+		/*
+		 * Compute output and apply limits
+		 */
+		out = proportional + _integrator + _differentiator;
 
-    if (config.output_limit_enable)
-    {
-        if (out > config.output_limit_max)
-        {
-            out = config.output_limit_max;
-        }
-        else if (out < config.output_limit_min)
-        {
-            out = config.output_limit_min;
-        }
-    }
+		if (_config.output_limit_enable)
+		{
+			if (out > _config.output_limit_max)
+			{
+				out = _config.output_limit_max;
+			}
+			else if (out < _config.output_limit_min)
+			{
+				out = _config.output_limit_min;
+			}
+		}
 
-    /* Store error and measurement for later use */
-    _prevError = error;
-    _prevMeasurement = measurement;
+		/* Store error and measurement for later use */
+		_prevError = error;
+		_prevMeasurement = measurement;
 
-    /* Return controller output */
-    return out;
-}
+		/* Return controller output */
+		return out;
+	}
 
 /**
  * @brief y = Kp(1 + Ki/s + Kd*s/(s*tua+1))
@@ -114,72 +119,72 @@ float PidController::update_parallel(float setpoint, float measurement)
  * @param measurement
  * @return float
  */
-float PidController::update_serial(float setpoint, float measurement)
-{
-    float out;
+	float PidController::update_serial(float setpoint, float measurement)
+	{
+		float out;
 
-    /*
-     * Error signal
-     */
-    float error = setpoint - measurement;
+		/*
+		 * Error signal
+		 */
+		float error = setpoint - measurement;
 
-    /*
-     * Proportional
-     */
-    float proportional = error;
+		/*
+		 * Proportional
+		 */
+		float proportional = error;
 
-    /*
-     * Integral
-     */
-    _integrator = _integrator + 0.5f * config.Ki * config.sample_time * (error + _prevError);
+		/*
+		 * Integral
+		 */
+		_integrator = _integrator + 0.5f * _config.Ki * _config.sample_time * (error + _prevError);
 
-    if (config.intergrator_limit_enable)
-    {
-        /* Anti-wind-up via _integrator clamping */
-        if (_integrator > config.intergrator_limit_max)
-        {
-            _integrator = config.intergrator_limit_max;
-        }
-        else if (_integrator < config.intergrator_limit_min)
-        {
-            _integrator = config.intergrator_limit_min;
-        }
-    }
+		if (_config.integrator_limit_enable)
+		{
+			/* Anti-wind-up via _integrator clamping */
+			if (_integrator > _config.integrator_limit_max)
+			{
+				_integrator = _config.integrator_limit_max;
+			}
+			else if (_integrator < _config.integrator_limit_min)
+			{
+				_integrator = _config.integrator_limit_min;
+			}
+		}
 
-    /*
-     * Derivative (band-limited _differentiator)
-     */
+		/*
+		 * Derivative (band-limited _differentiator)
+		 */
 
-    _differentiator =
-        -(2.0f * config.Kd * (measurement - _prevMeasurement)
-          /* Note: derivative on measurement, therefore minus sign in front of equation! */
-          + (2.0f * config.tau - config.sample_time) * _differentiator) /
-        (2.0f * config.tau + config.sample_time);
+		_differentiator =
+			-(2.0f * _config.Kd * (measurement - _prevMeasurement)
+				/* Note: derivative on measurement, therefore minus sign in front of equation! */
+				+ (2.0f * _config.tau - _config.sample_time) * _differentiator) /
+				(2.0f * _config.tau + _config.sample_time);
 
-    /*
-     * Compute output and apply limits
-     */
-    out = config.Kp * (proportional + _integrator + _differentiator);
+		/*
+		 * Compute output and apply limits
+		 */
+		out = _config.Kp * (proportional + _integrator + _differentiator);
 
-    if (config.output_limit_enable)
-    {
+		if (_config.output_limit_enable)
+		{
 
-        if (out > config.output_limit_max)
-        {
-            out = config.output_limit_max;
-        }
-        else if (out < config.output_limit_min)
-        {
-            out = config.output_limit_min;
-        }
-    }
+			if (out > _config.output_limit_max)
+			{
+				out = _config.output_limit_max;
+			}
+			else if (out < _config.output_limit_min)
+			{
+				out = _config.output_limit_min;
+			}
+		}
 
-    /* Store error and measurement for later use */
-    _prevError = error;
-    _prevMeasurement = measurement;
+		/* Store error and measurement for later use */
+		_prevError = error;
+		_prevMeasurement = measurement;
 
-    /* Return controller output */
-    return out;
-};
+		/* Return controller output */
+		return out;
+	};
 
 } // namespace wwControl
--- pid/pid.hpp
+++ pid/pid.hpp
@@ -1,61 +1,58 @@
 #ifndef __WWCONTROL_PID_HPP__
 #define __WWCONTROL_PID_HPP__
 
+#include "framework/framework.hpp"
+
 namespace wwControl
 {
+	enum class PidControllerMode
+	{
+		Serial,
+		Parallel,
+	};
+	struct PidControllerConfig
+	{
+		PidControllerMode mode;
+		/* Controller gains */
+		float Kp;
+		float Ki;
+		float Kd;
 
-class PidController
-{
-  public:
-    enum class Mode
-    {
-        Serial,
-        Parallel,
-    };
+		/* Derivative low-pass filter time constant */
+		float tau;
 
-    struct Config
-    {
-        Mode mode;
-        /* Controller gains */
-        float Kp;
-        float Ki;
-        float Kd;
+		/* Output limits */
+		bool output_limit_enable;
+		float output_limit_max;
+		float output_limit_min;
 
-        /* Derivative low-pass filter time constant */
-        float tau;
+		/* Integrator limits */
+		bool integrator_limit_enable;
+		float integrator_limit_max;
+		float integrator_limit_min;
 
-        /* Output limits */
-        bool output_limit_enable;
-        float output_limit_max;
-        float output_limit_min;
+		/* Sample time (in seconds) */
+		float sample_time;
+	};
 
-        /* Integrator limits */
-        bool intergrator_limit_enable;
-        float intergrator_limit_max;
-        float intergrator_limit_min;
+	class PidController : public Configurable<PidControllerConfig>
+	{
+	 public:
+		void reset();
+		float update(float setpoint, float measurement);
 
-        /* Sample time (in seconds) */
-        float sample_time;
-    };
+	 protected:
 
-  public:
-    PidController(Config &config) : config(config){};
-    PidController(Config &&config) : config(config){};
-    void reset();
-    float update(float setpoint, float measurement);
-    Config config;
+	 private:
+		float update_serial(float setpoint, float measurement);
+		float update_parallel(float setpoint, float measurement);
 
-  protected:
-  private:
-    float update_serial(float setpoint, float measurement);
-    float update_parallel(float setpoint, float measurement);
-
-    /* Controller "memory" */
-    float _integrator;
-    float _prevError; /* Required for _integrator */
-    float _differentiator;
-    float _prevMeasurement; /* Required for _differentiator */
-};
+		/* Controller "memory" */
+		float _integrator;
+		float _prevError; /* Required for _integrator */
+		float _differentiator;
+		float _prevMeasurement; /* Required for _differentiator */
+	};
 
 } // namespace wwControl
 
